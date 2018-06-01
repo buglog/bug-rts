ec065eaeb3766d0d2976184b0d8be7c0a8eb6d7a
ingor <ingoraschka@alum.calarts.edu>
2018-06-01 00:08:09 -0700

    added some bug funks

--- Engine/Bug.h
+++ Engine/Bug.h
@@ -1,2 +1,19 @@
 #pragma once
+#include "RectF.h"
+#include "Vec2.h"
+#include "Mouse.h"
 #include "Board.h"
+
+class Bug
+{
+public:
+	void Update(float dt, const Mouse& mouse);
+	void Draw(Graphics& gfx);
+	RectF GetRect();
+public:
+	Vec2 pos;
+	Vec2 vel;
+	Vec2 target;
+	static constexpr int width = 30;
+	static constexpr int height = 20;
+};
